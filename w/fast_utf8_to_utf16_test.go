43839ada85bb42ec73b895ebf07799af13715f5e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-04-20 22:59:44 -0700

    another test

--- w/fast_utf8_to_utf16_test.go
+++ w/fast_utf8_to_utf16_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestToUnicode(t *testing.T) {
+func TestToUnicodeShortLived(t *testing.T) {
 	s := "abc"
 	for i := 0; i < 13; i++ {
 		u := ToUnicodeShortLived(s)
@@ -17,5 +17,21 @@ func TestToUnicode(t *testing.T) {
 	assert.True(t, len(s) > stringAllocatorDefaultBufSize)
 	assert.Equal(t, nFrees, nFastFrees)
 	assert.Equal(t, stringAllocatorCurrPos, 0)
+}
+
+func TestToUnicodeShortLived2(t *testing.T) {
+	s := "abcdefgh"
+	nNotFast := 0
+	for i := 0; i < 1024; i++ {
+		u := ToUnicodeShortLived(s)
+		// from time to time simulate not freeing immediately
+		if i%4 == 0 {
+			ToUnicodeShortLived(s + "ha")
+			nNotFast++
+		}
+		FreeShortLivedUnicode(u)
+	}
+	assert.Equal(t, nFastFrees+nNotFast, nFrees)
+	assert.Equal(t, stringAllocatorCurrPos, (11+9)*nNotFast)
 }
 
